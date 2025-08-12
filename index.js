27cc9492af9e87d39337e6f29b750b661bb9d0a6
Albert Lash <albert.lash@gmail.com>
2025-08-12 13:26:53 -0400

    init

---
+++ index.js
@@ -0,0 +1,232 @@
+exports.tokens = [
+    {
+        symbol: "optaave",
+        chainId: 10,
+        address: "0x76FB31fb4af56892A25e32cFC43De717950c9278"
+    },
+    {
+        symbol: "aero",
+        address: "0x940181a94a35a4569e4529a3cdfb74e38fd98631",
+        chainId: 8453
+    },
+    {
+        symbol: "alb",
+        address: "0x1dd2d631c92b1acdfcdd51a0f7145a50130050c4",
+        chainId: 8453
+    },
+    {
+        symbol: "b3",
+        address: "0xb3b32f9f8827d4634fe7d973fa1034ec9fddb3b3",
+        chainId: 8453
+    },
+    {
+        symbol: "brett",
+        address: "0x532f27101965dd16442e59d40670faf5ebb142e4",
+        chainId: 8453
+    },
+    {
+        symbol: "cbada",
+        address: "0xcbADA732173e39521CDBE8bf59a6Dc85A9fc7b8c",
+        chainId: 8453
+    },
+    {
+        symbol: "cbbtc",
+        address: "0xcbB7C0000aB88B473b1f5aFd9ef808440eed33Bf",
+        decimals: 8,
+        chainId: 8453
+    },
+    {
+        symbol: "cbxrp",
+        address: "0xcb585250f852c6c6bf90434ab21a00f02833a4af",
+        decimals: 6,
+        chainId: 8453
+    },
+    {
+        symbol: "champs",
+        address: "0xeb6d78148f001f3aa2f588997c5e102e489ad341",
+        chainId: 8453
+    },
+    {
+        symbol: "clanker",
+        address: "0x1bc0c42215582d5a085795f4badbac3ff36d1bcb",
+        chainId: 8453
+    },
+    {
+        symbol: "lbtc",
+        address: "0xecAc9C5F704e954931349Da37F60E39f515c11c1",
+        decimals: 8,
+        chainId: 8453
+    },
+    {
+        symbol: "optlink",
+        address: "0x350a791Bfc2C21F9Ed5d10980Dad2e2638ffa7f6",
+        chainId: 10
+    },
+    {
+        symbol: "ldo",
+        address: "0xfdb794692724153d1488ccdbe0c56c252596735f",
+        chainId: 10
+    },
+    {
+        symbol: "lum",
+        address: "0x0fD7a301B51d0A83FCAf6718628174D527B373b6",
+        chainId: 8453
+    },
+    {
+        symbol: "benji",
+        address: "0xbc45647ea894030a4e9801ec03479739fa2485f0",
+        chainId: 8453
+    },
+    {
+        symbol: "luna",
+        address: "0x55cd6469f597452b5a7536e2cd98fde4c1247ee4",
+        chainId: 8453
+    },
+    {
+        symbol: "miggles",
+        address: "0xB1a03EdA10342529bBF8EB700a06C60441fEf25d",
+        chainId: 8453
+    },
+    {
+        symbol: "morpho",
+        address: "0xbaa5cc21fd487b8fcc2f632f3f4e8d37262a0842",
+        chainId: 8453
+    },
+    {
+        symbol: "moxie",
+        address: "0x8C9037D1Ef5c6D1f6816278C7AAF5491d24CD527",
+        chainId: 8453
+    },
+    {
+        symbol: "odos",
+        address: "0xca73ed1815e5915489570014e024b7ebe65de679",
+        chainId: 8453
+    },
+    {
+        symbol: "op",
+        address: "0x4200000000000000000000000000000000000042",
+        chainId: 10
+    },
+    {
+        symbol: "ovn",
+        address: "0xa3d1a8deb97b111454b294e2324efad13a9d8396",
+        chainId: 8453
+    },
+    {
+        symbol: "snx",
+        address: "0x8700dAec35aF8Ff88c16BdF0418774CB3D7599B4",
+        chainId: 10
+    },
+    {
+        symbol: "toshi",
+        address: "0xac1bd2486aaf3b5c0fc3fd868558b082a531b2b4",
+        chainId: 8453
+    },
+    {
+        symbol: "usdc",
+        currentPrice: 1,
+        stablecoin: true,
+        address: "0x176211869cA2b568f2A7D4EE941E073a821EE1ff",
+        decimals: 6
+    },
+    {
+        symbol: "usdbc",
+        currentPrice: 1,
+        stablecoin: true,
+        address: "0xd9aAEc86B65D86f6A7B5B1b0c42FFA531710b6CA",
+        decimals: 6
+    },
+    {
+        symbol: "usdcbase",
+        currentPrice: 1,
+        stablecoin: true,
+        address: "0x833589fcd6edb6e08f4c7c32d4f71b54bda02913",
+        decimals: 6
+    },
+    {
+        symbol: "velo",
+        address: "0x9560e827aF36c94D2Ac33a39bCE1Fe78631088Db",
+        chainId: 10
+    },
+    {
+        symbol: "virtual",
+        address: "0x0b3e328455c4059eeb9e3f84b5543f74e24e7e1b",
+        chainId: 8453
+    },
+    {
+        symbol: "wbtc",
+        address: "0x68f180fcce6836688e9084f035309e29bf0a2095",
+        decimals: 8,
+        chainId: 10
+    },
+    {
+        symbol: "baseusdc",
+        address: "0x833589fcd6edb6e08f4c7c32d4f71b54bda02913",
+        stablecoin: true,
+        currentPrice: 1,
+        decimals: 6
+    },
+    {
+        symbol: "baseweth",
+        address: "0x4200000000000000000000000000000000000006",
+        chainId: 8453
+    },
+    {
+        symbol: "optusdc",
+        stablecoin: true,
+        currentPrice: 1,
+        decimals: 6,
+        address: "0x0b2C639c533813f4Aa9D7837CAf62653d097Ff85"
+    },
+    {
+        symbol: "optusdt",
+        currentPrice: 1,
+        stablecoin: true,
+        address: "0x94b008aa00579c1307b0ef2c499ad98a8ce58e58",
+        decimals: 6
+    },
+    {
+        symbol: "optusdt0",
+        address: "0x01bff41798a0bcf287b996046ca68b395dbc1071",
+        currentPrice: 1,
+        stablecoin: true,
+        decimals: 6
+    },
+    {
+        symbol: "optwbtc",
+        address: "0x68f180fcCe6836688e9084f035309E29Bf0A2095",
+        decimals: 8,
+        chainId: 10
+    },
+    {
+        symbol: "optweth",
+        address: "0x4200000000000000000000000000000000000006",
+        chainId: 10
+    },
+    {
+        symbol: "uniuni",
+        address: "0x8f187aa05619a017077f5308904739877ce9ea21"
+    },
+    {
+        symbol: "uniusdc",
+        address: "0x078D782b760474a361dDA0AF3839290b0EF57AD6",
+        currentPrice: 1,
+        stablecoin: true,
+        decimals: 6
+    },
+    {
+        symbol: "well",
+        address: "0xA88594D404727625A9437C3f886C7643872296AE",
+        chainId: 8453
+    },
+    {
+        symbol: "wld",
+        address: "0xdc6ff44d5d932cbd77b52e5612ba0529dc6226f1",
+        chainId: 10
+    },
+    {
+        symbol: "zora",
+        address: "0x1111111111166b7fe7bd91427724b487980afc69",
+        chainId: 8453
+    }
+]
